2ac7e6b0af10b3906a6be52486914b0e75ecae10
qbamms <111325220+qbamms@users.noreply.github.com>
2022-12-31 04:12:11 +0000

    deel.sh has been updated with new feature.

--- deel.sh
+++ deel.sh
@@ -1,1 +1,4 @@
 echo "i have started applying for DevOps jobs and i know by the grace of God i will get a better offer soon"
+echo "heaven helps those who help themselves"
+echo "you have to prepare, do your research, study hard and believe in God"
+echo "everything will eventually work out and you will definetly get the job by His grace."
